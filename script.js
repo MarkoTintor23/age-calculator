982c532469c1bda8b1b0a8834c04095e4ad3b76d
Marko Tintor <marko.tintor.18@singimail.rs>
2025-10-06 15:50:47 +0200

    doodle

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let brojevi = [16, 45, 12, 1, 2, 66];
-
 // //poslednja cifra broja =====================================
 
 // function pom(broj) {
@@ -91,10 +89,211 @@ let brojevi = [16, 45, 12, 1, 2, 66];
 
 // //===================================================================================================================
 
-function pom(broj) {
-  return broj % 10;
-}
+// function pom(broj) {
+//   return broj % 10;
+// }
 
+// function glavna(niz) {
+//   let sum = 0;
+//   for (let i = 0; i < niz.length; i++) {
+//     sum += pom(niz[i]);
+//   }
+//   return sum;
+// }
+// console.log(glavna(brojevi));
+// //=====================================================
+// function stringovi(str) {
+//   let noviStr = [];
+//   for (let i = 0; i < str.length; i += 2) {
+//     if (i + 1 < str.length) {
+//       noviStr += str[i + 1] + str[i];
+//     } else {
+//       noviStr += str[i];
+//     }
+//   }
+//   return noviStr;
+// }
+// let zadatak = "abcd13493kfdk";
+// console.log(stringovi(zadatak));
+
+// //........................................................
+
+// function test(broj) {
+//   if (broj > 0 && broj < 10) {
+//     return 0;
+//   } else {
+//     let floor = Math.floor(broj / 10);
+//     let digit = floor % 10;
+//     return digit;
+//   }
+// }
+// function glavni2(niz) {
+//   let sum = 0;
+//   for (let i = 0; i < niz.length; i++) {
+//     sum += test(niz[i]);
+//   }
+//   return sum;
+// }
+// console.log(glavni2(brojevi));
+// //.........................................................
+
+// function stringovi2(str) {
+//   let noviStr = [];
+
+//   for (let i = 0; i < str.length; i++) {
+//     noviStr += str[i] + str[i];
+//   }
+//   return noviStr;
+// }
+
+// console.log(stringovi2(zadatak));
+
+// //./////////////////////////////////////////////////////////
+
+// function pom2(broj) {
+//   if (broj < 10) {
+//     return broj;
+//   }
+//   while (broj >= 10) {
+//     broj = Math.floor(broj / 10);
+//   }
+//   return broj;
+// }
+// function glavna3(niz) {
+//   let sum = 0;
+
+//   for (let i = 0; i < niz.length; i++) {
+//     sum += pom2(niz[i]);
+//   }
+//   return sum;
+// }
+
+// console.log(glavna3(brojevi));
+
+// //.............................................................
+
+// function test2(broj) {
+//   let sum = 0;
+
+//   while (broj > 0) {
+//     let poslednjiBroj = broj % 10;
+//     if (poslednjiBroj % 2 === 0) {
+//       sum += poslednjiBroj;
+//     }
+//     broj = Math.floor(broj / 10);
+//   }
+//   return sum;
+// }
+
+// function glavna4(niz) {
+//   let sum = 0;
+
+//   for (let i = 0; i < niz.length; i++) {
+//     sum += test2(niz[i]);
+//   }
+//   return sum;
+// }
+
+// console.log(glavna4(brojevi));
+
+// //.......................................................
+// function stringovi3(str) {
+//   let noviStr = [];
+//   for (let i = 0; i < str.length; i++) {
+//     if (i % 2 === 0) {
+//       noviStr += str[i];
+//     }
+//   }
+//   return noviStr;
+// }
+
+// console.log(stringovi3(zadatak), zadatak);
+
+// //./..................................................
+// let sum = 0;
+// let mat = [
+//   [2, 5, 7],
+//   [1, 3, 9],
+// ];
+// for (let i = 0; i < mat.length; i++) {
+//   for (let j = 0; j < mat[i].length; j++) {
+//     console.log(mat[i][j]);
+//   }
+// }
+// for (let i = 0; i < mat.length; i++) {
+//   for (let j = 0; j < mat[i].length; j++) {
+//     sum += mat[i][j];
+//   }
+// }
+// console.log(sum);
+
+// let mat2 = [];
+
+// for (let i = 0; i < mat.length; i++) {
+//   let red = [];
+//   for (let j = 0; j < mat[i].length; j++) {
+//     red.push(mat[i][j] * 2);
+//   }
+//   mat2.push(red);
+// }
+// console.log(mat2);
+
+// function matricaNove(matrica) {
+//   let newMat = [];
+//   for (let i = 0; i < matrica.length; i++) {
+//     let red = [];
+//     for (let j = 0; j < matrica[i].length; j++) {
+//       if (matrica[i][j] > 5) {
+//         red.push(9 - matrica[i][j]);
+//       } else {
+//         red.push(matrica[i][j]);
+//       }
+//     }
+//     newMat.push(red);
+//   }
+//   return newMat;
+// }
+
+// console.log(matricaNove(mat));
+
+// function matricaDruga(matrica) {
+//   return matrica.map((red) => red.map((x) => (x < 5 ? x + 5 : x)));
+// }
+// console.log(matricaDruga(mat));
+
+// function figura(n) {
+//   const ukupanBrRedova = 2 * n;
+
+//   for (let i = 0; i < ukupanBrRedova; i++) {
+//     const k = Math.floor(i / 2) + 1;
+//     let linija = "";
+
+//     if (i % 2 === 0) {
+//       linija += "*".repeat(k);
+//       linija += "-".repeat(n - k);
+//     } else {
+//       linija += "-".repeat(k);
+//       linija += "*".repeat(n - k);
+//     }
+
+//     console.log(linija);
+//   }
+// }
+// console.log(figura(5));
+let brojevi = [16, 45, 12, 1, 2, 66];
+
+let mat = [
+  [2, 5, 7, 9],
+  [1, 3, 9, 9],
+  [2, 5, 7, 9],
+  [2, 5, 7, 9],
+];
+
+let zadatak = "abcd13493kfdk";
+
+function pom(broj) {
+  return Math.abs(broj % 10);
+}
 function glavna(niz) {
   let sum = 0;
   for (let i = 0; i < niz.length; i++) {
@@ -102,111 +301,85 @@ function glavna(niz) {
   }
   return sum;
 }
+
 console.log(glavna(brojevi));
-//=====================================================
-function stringovi(str) {
-  let noviStr = [];
+
+function matrica(mat) {
+  return mat.map((red) => red.map((x) => (x > 5 ? 9 - x : x)));
+}
+console.log(matrica(mat));
+
+function noviStr(str) {
+  let stringe = [];
+
   for (let i = 0; i < str.length; i += 2) {
     if (i + 1 < str.length) {
-      noviStr += str[i + 1] + str[i];
+      stringe += str[i + 1] + str[i];
     } else {
-      noviStr += str[i];
+      stringe += str[i];
     }
   }
-  return noviStr;
+  return stringe;
 }
-let zadatak = "abcd13493kfdk";
-console.log(stringovi(zadatak));
+console.log(noviStr(zadatak), zadatak);
 
-//........................................................
-
-function test(broj) {
+function predzadnja(broj) {
   if (broj > 0 && broj < 10) {
     return 0;
-  } else {
-    let floor = Math.floor(broj / 10);
-    let digit = floor % 10;
-    return digit;
   }
+  let floor = Math.floor(broj / 10);
+  let digit = floor % 10;
+  return digit;
 }
-function glavni2(niz) {
+function glavna2(niz) {
   let sum = 0;
   for (let i = 0; i < niz.length; i++) {
-    sum += test(niz[i]);
+    sum += predzadnja(niz[i]);
   }
   return sum;
 }
-console.log(glavni2(brojevi));
-//.........................................................
+console.log(glavna2(brojevi));
 
-function stringovi2(str) {
-  let noviStr = [];
-
-  for (let i = 0; i < str.length; i++) {
-    noviStr += str[i] + str[i];
-  }
-  return noviStr;
+function matricaaaa(mat) {
+  return mat.map((red) => red.map((x) => (x < 5 ? x + 5 : x)));
 }
+console.log(matricaaaa(mat));
 
-console.log(stringovi2(zadatak));
-
-//./////////////////////////////////////////////////////////
-
-function pom2(broj) {
-  if (broj < 10) {
-    return broj;
+function stringeri(str) {
+  let novi = [];
+  for (let i = 0; i < str.length; i++) {
+    novi += str[i] + str[i];
   }
+  return novi;
+}
+console.log(stringeri(zadatak), zadatak);
+
+function prvi(broj) {
   while (broj >= 10) {
     broj = Math.floor(broj / 10);
   }
   return broj;
 }
-function glavna3(niz) {
-  let sum = 0;
 
+function glavna5(niz) {
+  let sum = 0;
   for (let i = 0; i < niz.length; i++) {
-    sum += pom2(niz[i]);
+    sum += prvi(niz[i]);
   }
   return sum;
 }
 
-console.log(glavna3(brojevi));
+console.log(glavna5(brojevi));
 
-//.............................................................
-
-function test2(broj) {
-  let sum = 0;
-
-  while (broj > 0) {
-    let poslednjiBroj = broj % 10;
-    if (poslednjiBroj % 2 === 0) {
-      sum += poslednjiBroj;
-    }
-    broj = Math.floor(broj / 10);
-  }
-  return sum;
+function prviPoslednji(matr) {
+  let bezPrvogReda = matr.slice(1, matr.length - 1);
+  let delu = bezPrvogReda.map((red) => red.slice(1, matr.length - 1));
+  return delu;
 }
 
-function glavna4(niz) {
-  let sum = 0;
+console.log(prviPoslednji(mat));
 
-  for (let i = 0; i < niz.length; i++) {
-    sum += test2(niz[i]);
+function parni(broj) {
+  if (broj % 2 === 0) {
   }
-  return sum;
 }
-
-console.log(glavna4(brojevi));
-
-//.......................................................
-function stringovi3(str) {
-  let noviStr = [];
-  for (let i = 0; i < str.length; i++) {
-    if (i % 2 === 0) {
-      noviStr += str[i];
-    }
-  }
-  return noviStr;
-}
-
-console.log(stringovi3(zadatak), zadatak);
